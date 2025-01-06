0126579bef5595955611c75688d035a0a104054b
Glory Chicaté <74722393+Mykcy@users.noreply.github.com>
2025-01-06 23:04:57 +0100

    Update example.py

--- example.py
+++ example.py
@@ -82,9 +82,12 @@ if __name__ == "__main__":
 
 
         # For remote server only (AWS)
-        remote_server_uri = "http://ec2-54-147-36-34.compute-1.amazonaws.com:5000/"
+        remote_server_uri = "https://dagshub.com/glory.chicate/mlflow_mvc.mlflow"
         mlflow.set_tracking_uri(remote_server_uri)
 
+
+
+        
 
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
